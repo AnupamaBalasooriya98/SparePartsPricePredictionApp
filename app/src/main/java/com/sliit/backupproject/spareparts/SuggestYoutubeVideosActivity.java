e9386438eaa4e9d0852bc73bc460fd3f227fe234
Balasooriya S. A. - IT17184922 <it17184922@my.sliit.lk>
2021-07-05 02:53:48 +0530

    Done youtube video suggesion implementation

--- app/src/main/java/com/sliit/backupproject/spareparts/SuggestYoutubeVideosActivity.java
+++ app/src/main/java/com/sliit/backupproject/spareparts/SuggestYoutubeVideosActivity.java
@@ -1,9 +1,13 @@
 package com.sliit.backupproject.spareparts;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 import com.sliit.backupproject.R;
 
 public class SuggestYoutubeVideosActivity extends AppCompatActivity {
@@ -12,5 +16,17 @@ public class SuggestYoutubeVideosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggest_youtube_videos);
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "p3CcfIjycBA";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+
     }
 }
