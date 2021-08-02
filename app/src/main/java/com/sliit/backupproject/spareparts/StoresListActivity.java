cf0aeeedebf0b593cf182b82801a8ce8bae8c94a
Balasooriya S. A. - IT17184922 <it17184922@my.sliit.lk>
2021-08-02 15:38:19 +0530

    Create new activity to display stores list

---
+++ app/src/main/java/com/sliit/backupproject/spareparts/StoresListActivity.java
@@ -0,0 +1,16 @@
+package com.sliit.backupproject.spareparts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sliit.backupproject.R;
+
+public class StoresListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stores_list);
+    }
+}
